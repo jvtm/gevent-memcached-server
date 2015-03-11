fa078d79734cb51de0081a03ce434c61a0500092
Jyrki Muukkonen <jyrki.muukkonen@f-secure.com>
2015-03-12 01:50:02 +0200

    more setup.py fields

--- setup.py
+++ setup.py
@@ -2,8 +2,21 @@ from setuptools import setup, find_packages
 from gmcbs import __version__
 
 setup(
-    name="gevent-memcached-server",
+    name='gevent-memcached-server',
     version=__version__,
+    description='gevent based memcached binary protocol server',
+    url='https://github.com/jvtm/gevent-memcached-server',
+    author='Jyrki Muukkonen',
+    author_email='jvtm@kruu.org',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='gevent memcached memcache binary protocol server',
     packages=find_packages(),
     install_requires=['gevent'],
     entry_points={
