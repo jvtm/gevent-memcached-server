1a2159c260c417059fe4c88ceead4f0ffae5a20f
Jyrki Muukkonen <jyrki.muukkonen@f-secure.com>
2015-03-12 23:08:09 +0200

    try to support tcp keepalive on osx

--- gmcbs/server.py
+++ gmcbs/server.py
@@ -10,7 +10,22 @@ gevent.monkey.patch_thread()
 from gevent.server import StreamServer
 from gmcbs.protocol import ClientHandler
 import logging
+import socket
 import sys
+
+
+def tcp_keepalive_linux(sock, keepidle, keepintvl, keepcnt):
+    """ Set TCP keepalive options on Linux """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.SOL_TCP, socket.TCP_KEEPIDLE, keepidle)
+    sock.setsockopt(socket.SOL_TCP, socket.TCP_KEEPINTVL, keepintvl)
+    sock.setsockopt(socket.SOL_TCP, socket.TCP_KEEPCNT, keepcnt)
+
+
+def tcp_keepalive_osx(sock, keepidle, keepintvl, keepcnt):
+    """ Set TCP keepalive options on MacOSX (and maybe real BSDs?) """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, 0x10, keepintvl)
 
 
 class ConnectionHelper(object):
@@ -18,21 +33,24 @@ class ConnectionHelper(object):
     # this is still a class so that we can pass in config dict and others
     def __init__(self, handler_class=ClientHandler, handler_kwargs=None, tcp_keepalive=True):
         self.log = logging.getLogger(self.__class__.__name__)
+        # XXX: this is still a bit hacky. find out if anyone has made a true cross-platform tcp keepalive setting
         if tcp_keepalive is True:
             tcp_keepalive = (1800, 30, 3)
-        self.tcp_keepalive = tcp_keepalive
+        if tcp_keepalive:
+            if sys.platform == "darwin":
+                self.set_keepalive = lambda x: tcp_keepalive_osx(x, *tcp_keepalive)
+            else:
+                # sys.platform == "linux2"
+                self.set_keepalive = lambda x: tcp_keepalive_linux(x, *tcp_keepalive)
+        else:
+            self.set_keepalive = lambda x: None
         self.handler_class = handler_class
         self.handler_kwargs = dict(handler_kwargs or [])
 
     def __call__(self, sock, addr):
         addrinfo = "%s:%s" % addr
         self.log.info("connected %s", addrinfo)
-        if self.tcp_keepalive:
-            import socket
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-            sock.setsockopt(socket.SOL_TCP, socket.TCP_KEEPIDLE, self.tcp_keepalive[0])
-            sock.setsockopt(socket.SOL_TCP, socket.TCP_KEEPINTVL, self.tcp_keepalive[1])
-            sock.setsockopt(socket.SOL_TCP, socket.TCP_KEEPCNT, self.tcp_keepalive[2])
+        self.set_keepalive(sock)
         handler = self.handler_class(sock, **self.handler_kwargs)
         try:
             handler.serve_client()
